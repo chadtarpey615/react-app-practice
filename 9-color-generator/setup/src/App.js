88991189454493bd81b1ea3016d12a1c45de4729
Chad Tarpey <chadtarpey@gmail.com>
2021-03-10 15:47:26 -0600

    finshed singleColor.js

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f32145").all(10));
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
@@ -29,7 +29,7 @@ function App() {
           <input type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder="#f15025"
+            placeholder="#f32145"
             className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
@@ -39,7 +39,7 @@ function App() {
       </section>
       <section className="colors">
         {list.map((color, index) => {
-          return <SingleColor key={index} {...color} index={index} />
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex} />
         })}
       </section>
     </>
