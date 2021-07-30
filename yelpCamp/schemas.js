29416f79688a391112a6a5d5bbeb19a246573939
Chad Tarpey <chadtarpey@gmail.com>
2021-07-29 19:04:48 -0500

    displaying all raitngs for campgrounds on show.ejs

--- yelpCamp/schemas.js
+++ yelpCamp/schemas.js
@@ -15,6 +15,6 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: jsxOpeningElement.string().required()
+        body: Joi.string().required()
     }).required()
 })
