94af78740660d9e09a41f537eef1acd80801afca
Chad Tarpey <chadtarpey@gmail.com>
2021-08-02 11:48:52 -0500

    installed express-session and created public folder

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require("path");
 const mongoose = require('mongoose');
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError")
-const methodOverride = require("method-Override")
+const session = require("express-session");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-Override");
+
+
 
 const campgrounds = require("./routes/campgrounds")
 const reviews = require("./routes/reviews")
@@ -11,7 +14,8 @@ const reviews = require("./routes/reviews")
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -28,7 +32,20 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+    secret: "thisshouldbeabettersecret!",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+}
+app.use(session(sessionConfig));
 
 
 
