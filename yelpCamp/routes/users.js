f91178e9a879029f569671e5b47ac41b528ddf21
Chad Tarpey <chadtarpey@gmail.com>
2021-08-03 13:25:50 -0500

    created login.ejs and logging in with passport authenticate

--- yelpCamp/routes/users.js
+++ yelpCamp/routes/users.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const passport = require("passport")
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
     res.render("users/register")
 });
 
-router.post("/register", async (req, res) => {
-    res.send(req.body)
+
+// creating a new user to sign in 
+router.post("/register", catchAsync(async (req, res) => {
+    try {
+        // destructure what we need from the req.body from the form 
+        const { email, username, password } = req.body;
+        // making a new User
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash("Welcome to yelp camp");
+        res.redirect("/campgrounds")
+    }
+    catch (err) {
+        req.flash("error", err.message)
+        res.redirect("register")
+
+    }
+
+}));
+
+router.get("/login", (req, res) => {
+    res.render("users/login");
+});
+
+
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+    req.flash("success", "Welcome back");
+    res.redirect("/campgrounds")
 })
 
 module.exports = router;
