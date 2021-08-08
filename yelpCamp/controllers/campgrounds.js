7f40de0eeb86069e30e2afa5ae983dd118ec6307
Chad Tarpey <chadtarpey@gmail.com>
2021-08-08 16:54:31 -0500

    added delete images on the edit.ejs that deletes from mongo and cloudinary

--- yelpCamp/controllers/campgrounds.js
+++ yelpCamp/controllers/campgrounds.js
@@ -1,5 +1,5 @@
 const Campground = require("../models/campground")
-
+const { cloudinary } = require("../cloudinary")
 
 module.exports.index = async (req, res) => {
 
@@ -56,7 +56,14 @@ module.exports.updateCampground = async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
-    campground.save()
+    await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            cloudinary.uploader.destroy(filename)
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+        console.log("campcampcamp::::: ", campground)
+    }
     req.flash("success", "Successfully updated campground!")
     res.redirect(`/campgrounds/${campground._id}`)
 }
