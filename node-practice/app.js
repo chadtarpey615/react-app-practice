ce9ed2ffcd09a8d2553ea434ddf6554ee9278a9c
Chad Tarpey <chadtarpey@gmail.com>
2021-08-11 13:46:11 -0500

    created home and contact.ejs files

--- node-practice/app.js
+++ node-practice/app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const ejsMate = require("ejs-mate");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const PORT = 4000
@@ -10,7 +11,8 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate)
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
