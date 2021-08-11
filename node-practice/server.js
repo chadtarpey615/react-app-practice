41e736ffe19e1fcd28c9d8a21126519d653378ab
Chad Tarpey <chadtarpey@gmail.com>
2021-08-10 20:42:52 -0500

    installed ejs and created navbar.ejs file

--- node-practice/server.js
+++ node-practice/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const PORT = 4000;
+// const PORT = 4000;
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World!!!")
-})
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}  🌍`)
-});
+// app.get("/", (req, res) => {
+//     res.send("Hello World!!!")
+// })
+// app.listen(PORT, () => {
+//     console.log(`App listening on port ${PORT}  🌍`)
+// });
 
