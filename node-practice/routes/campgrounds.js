e6a2b49d37fdb6088d8fc8db7bb94d6759fcfb78
Chad Tarpey <chadtarpey@gmail.com>
2021-08-13 14:57:14 -0500

    campground now saving successfully to mongo

--- node-practice/routes/campgrounds.js
+++ node-practice/routes/campgrounds.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const Campground = require("../models/campground");
-const { campgroundSchema } = require('../../yelpCamp/schemas');
+// const campground = require('../../yelpCamp/schemas');
 
 router.get("/new", (req, res) => {
     res.render("campgrounds/new")
 });
 
-router.post("/new", async (req, res) => {
-    const campground = new Campground({
-        title: req.body.title,
-        price: req.body.price,
-        description: req.body.description,
-        location: req.body.location,
+router.post("/", async (req, res) => {
+    // const campground = new Campground({
+    //     title: req.body.title,
+    //     price: req.body.price,
+    //     description: req.body.description,
+    //     location: req.body.location,
 
-    });
+    // });
+    const campground = new Campground(req.body.campground)
     console.log("did it save", campground);
 
     await campground.save();
+    res.redirect("campgrounds/new")
+    console.log(req.body.campground)
 })
 
 module.exports = router;
