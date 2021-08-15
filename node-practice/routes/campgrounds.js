44091dd159c62277d062412645dc2e8eabe3d128
Chad Tarpey <chadtarpey@gmail.com>
2021-08-15 17:18:09 -0500

    created show.ejs for finding campground by id and displaying single campground info

--- node-practice/routes/campgrounds.js
+++ node-practice/routes/campgrounds.js
@@ -12,12 +12,18 @@ router.get("/", async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render("campgrounds/index", { campgrounds })
     // console.log("campground/index", campground)
+});
+
+router.get("/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    console.log("$$$$$$$$$$$$", campground._id)
+    res.render("campgrounds/show", { campground });
 })
 
 router.post("/", async (req, res) => {
 
     const campground = new Campground(req.body.campground)
-    console.log("did it save", campground);
+    // console.log("did it save", campground);
 
     await campground.save();
     res.redirect("campgrounds/new")
