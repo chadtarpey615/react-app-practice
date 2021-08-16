53c56cd522f2b193211cf48d7277f23296b8ef8e
Chad Tarpey <chadtarpey@gmail.com>
2021-08-16 17:01:34 -0500

    successfully register new users on sign up page

--- node-practice/routes/users.js
+++ node-practice/routes/users.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/users");
+
+
 
 router.get("/register", (req, res) => {
   res.render("users/register")
 });
+
+router.post("/register", async (req, res) => {
+  const { email, username, password } = req.body;
+  const newUser = await new User({ email, username })
+  await newUser.save();
+  res.redirect("/campgrounds")
+})
 
 module.exports = router;
