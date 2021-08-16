1dab465739e8d2f8f10cc57f1031d37b9978612a
Chad Tarpey <chadtarpey@gmail.com>
2021-08-16 14:36:23 -0500

    added a image string to the model schema and else if in the single campgrounds to fall back on default img if none

--- node-practice/models/campground.js
+++ node-practice/models/campground.js
@@ -5,7 +5,8 @@ const CampgroundSchema = new mongoose.Schema({
     title: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    image: String
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
