0a54402715546dd8edaeab44a711ffa21b75da62
Chad Tarpey <chadtarpey@gmail.com>
2021-08-29 20:26:07 -0500

    created showRepos and clearAlerts methods

--- github-finder/app.js
+++ github-finder/app.js
@@ -1,3 +1,4 @@
+
 // init github class
 const github = new GitHub;
 
@@ -19,13 +20,15 @@ searchUser.addEventListener("keyup", e => {
             .then(data => {
                 if (data.profile.message === "Not Found") {
                     //show alert
-
+                    ui.showAlert("User not found", "alert alert-danger")
                 } else {
                     // show profile
                     ui.showProfile(data.profile);
+                    ui.showRepos(data.repos)
                 }
             })
     } else {
         // clear profile
+        ui.clearProfile();
     }
 })
--- github-finder/github.js
+++ github-finder/github.js
@@ -2,15 +2,24 @@ class GitHub {
     constructor() {
         this.client_id = "f494767dceea753c6b60";
         this.client_secret = " 28d338a691cd9ea3d19052f254e49baa5e74991d";
+        this.repos_count = 5;
+        this.repo_sort = "created: asc";
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repo_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+
 
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
 
         return {
-            profile
+            profile,
+            repos
         }
     }
+
+
 }
