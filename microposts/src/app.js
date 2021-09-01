1983c906ba15e2e7949342ba19703910c513c0e9
Chad Tarpey <chadtarpey@gmail.com>
2021-08-31 20:05:05 -0500

    created clearFields, clearAlert, and showAlert methods for post is added

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -4,8 +4,33 @@ import { ui } from "./ui"
 // get post om dom load
 document.addEventListener("DOMContentLoaded", getPosts);
 
+
+// listen for add posts
+document.querySelector(".post-submit").addEventListener("click", submitPost);
+
+
 function getPosts() {
     http.get("http://localhost:3000/posts")
         .then(data => ui.showPosts(data))
         .catch(error => console(error))
+}
+
+//submit post
+function submitPost() {
+    const title = document.querySelector("#title").value;
+    const body = document.querySelector("#body").value;
+
+    const data = {
+        title,
+        body
+    }
+
+    // create post
+    http.post("http://localhost:3000/posts", data)
+        .then(data => {
+            ui.showAlert("Post added", "alert alert-success");
+            ui.clearFields();
+            getPosts();
+        })
+        .catch(error => console.log(err))
 }
--- microposts/src/ui.js
+++ microposts/src/ui.js
@@ -31,6 +31,43 @@ class UI {
 
         this.post.innerHTML = output;
     }
+
+    showAlert(message, className) {
+        this.clearAlert();
+
+        // create a div
+        const div = document.createElement("div");
+        // add classes
+        div.className = className;
+        // add text
+        div.appendChild(document.createTextNode(message));
+        // get the parent 
+        const container = document.querySelector(".postsContainer");
+        // post div
+        const posts = document.querySelector("#posts");
+        //insert alert div
+        container.insertBefore(div, posts);
+
+
+        //timeout
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000)
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector(".alert");
+
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearFields() {
+        this.titleInput.value = "";
+        this.bodyInput.value = "";
+
+    }
 }
 
 export const ui = new UI();
