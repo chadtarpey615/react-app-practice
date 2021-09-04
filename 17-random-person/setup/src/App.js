9fcae281a9265b1ed61ec123c71729ff77ac6bcb
Chad Tarpey <chadtarpey@gmail.com>
2021-09-04 11:28:04 -0500

    created index.html and cdn for materialize css for task list

--- 17-random-person/setup/src/App.js
+++ 17-random-person/setup/src/App.js
@@ -7,9 +7,22 @@ import {
   FaPhone,
   FaLock,
 } from 'react-icons/fa'
-const url = 'https://randomuser.me/api/'
-const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+
+
+
+
 function App() {
+  const url = 'https://randomuser.me/api/'
+  const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+
+  useEffect(() => {
+    findPeople();
+  }, [])
+  const findPeople = async () => {
+    const data = await fetch(url)
+      .then((data) => console.log(data))
+  }
+
   return <h2>random user starter</h2>
 }
 
