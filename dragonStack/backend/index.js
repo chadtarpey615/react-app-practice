1fd5dfa5b08e1aeffe942e13264ab5307affd66f
Chad Tarpey <chadtarpey@gmail.com>
2021-09-26 15:38:58 -0500

    created traits.json file

--- dragonStack/backend/index.js
+++ dragonStack/backend/index.js
@@ -1,7 +1,13 @@
 const Dragon = require("./dragon.js")
 
-const fooey = new Dragon(new Date(), "fooey")
-const baloo = new Dragon(new Date(), "baloo")
+const fooey = new Dragon({ birthdate: new Date(), nickname: "fooey" })
+const baloo = new Dragon({ birthdate: new Date(), nickname: "baloo" })
+
+
+setTimeout(() => {
+    const tip = new Dragon()
+    console.log("tip", tip)
+}, 3000)
 
 console.log("fooey", fooey)
 console.log("baloo", baloo)
