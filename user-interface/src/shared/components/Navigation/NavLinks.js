cd74dc0e0fe49ae25d85e65dcf7e6bfd2ad13c15
Chad Tarpey <chadtarpey@gmail.com>
2021-10-26 17:07:28 -0500

    created UserPlaces.js and PlaceList.js files

--- user-interface/src/shared/components/Navigation/NavLinks.js
+++ user-interface/src/shared/components/Navigation/NavLinks.js
@@ -6,7 +6,16 @@ const NavLinks = (props) => {
     return (
         <ul className="nav-links">
             <li>
-                <NavLinks to="/"></NavLinks>
+                <NavLink to="/" exact>ALL USERS</NavLink>
+            </li>
+            <li>
+                <NavLink to="/u1/places">MY PLACES</NavLink>
+            </li>
+            <li>
+                <NavLink to="/places/new">ADD PLACES</NavLink>
+            </li>
+            <li>
+                <NavLink to="/auth">AUTHENTICATE</NavLink>
             </li>
         </ul>
     )
