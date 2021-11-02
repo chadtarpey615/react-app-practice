ebf0fba02240b74789a0c2a994c8ce0ef26064be
Chad Tarpey <chadtarpey@gmail.com>
2021-11-02 11:05:01 -0500

    have a http request to get all places by userId

--- user-interface/src/places/pages/UserPlaces.js
+++ user-interface/src/places/pages/UserPlaces.js
@@ -1,37 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList"
+import { useHttpClient } from "../../shared/hooks/http-hook"
+import ErrorModal from "../../shared/components/UIElements/ErrorModal"
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner"
 
-const dummyPlaces = [
-    {
-        id: "p1",
-        title: "Empire State Building",
-        description: "One of the most famous sku scrapers in the world!",
-        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAe1Lg2g2LI2TFIpcJYW5b-XcP8Bg8fFjG9w&usqp=CAU",
-        address: "20 W 34th St, New York, NY 10001",
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: "u1"
-    },
-    {
-        id: "p2",
-        title: "Empire State Building",
-        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAe1Lg2g2LI2TFIpcJYW5b-XcP8Bg8fFjG9w&usqp=CAU",
-        address: "20 W 34th St, New York, NY 10001",
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: "u2"
-    }
-]
+
 const UserPlaces = () => {
+    const [loadedPlaces, setLoadedPlaces] = useState()
+    const { isLoading, error, sendRequest, clearError } = useHttpClient()
+
+
     const userId = useParams().userId;
-    const loadedPlaces = dummyPlaces.filter(place => place.creator === userId)
+
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try {
+                const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`)
+                setLoadedPlaces(responseData.places)
+
+            } catch (error) {
+
+            }
+        }
+        fetchPlaces()
+    }, [sendRequest, userId])
+
+
     return (
-        <PlaceList items={loadedPlaces} />
+        <>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && <div className="center">
+                <LoadingSpinner />
+            </div>}
+            {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+
+        </>
     )
 }
 
