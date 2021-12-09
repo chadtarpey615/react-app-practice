57aa62fa70fc5c22b41e017c81e493c2e17ddc80
Chad Tarpey <chadtarpey@gmail.com>
2021-12-09 17:11:37 -0600

    installed react-router-dom and learned some new stuff about version6

--- brad-traversy-react-front-to-back-2022/feedback-app/src/App.js
+++ brad-traversy-react-front-to-back-2022/feedback-app/src/App.js
@@ -1,9 +1,12 @@
+import { v4 as uuidv4 } from "uuid"
 import React, { useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from "./pages/AboutPage"
 
 
 const App = () => {
@@ -17,16 +20,29 @@ const App = () => {
         }
     }
 
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     return (
-        <>
+        <Router>
             <Header />
             <div className="container">
-                <FeedbackForm />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                <Routes>
 
+                    <Route exact path="/" element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                        </>
+                    }>
+                    </Route>
+                    <Route path="/about" element={<AboutPage />} />
+                </Routes>
             </div>
-        </>
+        </Router>
     )
 }
 
--- brad-traversy-react-front-to-back-2022/feedback-app/src/components/FeedbackForm.jsx
+++ brad-traversy-react-front-to-back-2022/feedback-app/src/components/FeedbackForm.jsx
@@ -1,16 +1,44 @@
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
-function FeedbackForm() {
+import RatingSelect from "./RatingSelect";
+function FeedbackForm({ handleAdd }) {
     const [text, setText] = useState("");
+    const [rating, setRating] = useState(10);
+    const [btnDisabled, settnDisabled] = useState(true);
+    const [message, setMessage] = useState("");
 
     const handleTextChange = (e) => {
+        if (text === "") {
+            settnDisabled(true);
+            setMessage(null);
+        } else if (text !== "" && text.trim().length <= 10) {
+            settnDisabled(true);
+            setMessage("Text must be at least 10 characters");
+        } else {
+            setMessage(null);
+            settnDisabled(false);
+        }
         setText(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text.trim().length > 10) {
+            const newFeedBack = {
+                text,
+                rating,
+            };
+
+            handleAdd(newFeedBack);
+            setText("");
+        }
     };
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
+                <RatingSelect select={(rating) => setRating(rating)} />
                 <div className='input-group'>
                     <input
                         onChange={handleTextChange}
@@ -18,8 +46,12 @@ function FeedbackForm() {
                         placeholder='Write a review'
                         value={text}
                     />
-                    <Button type='submit'>Send</Button>
+                    <Button type='submit' isDisabled={btnDisabled}>
+                        Send
+                    </Button>
                 </div>
+
+                {message && <div className='message'>{message}</div>}
             </form>
         </Card>
     );
