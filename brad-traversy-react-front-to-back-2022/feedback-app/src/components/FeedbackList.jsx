f74ab2bfeb4a8df92b2865545ed37c3821b400e2
Chad Tarpey <chadtarpey@gmail.com>
2021-12-09 14:09:23 -0600

    created FeedbackItem and FeedbackList components

---
+++ brad-traversy-react-front-to-back-2022/feedback-app/src/components/FeedbackList.jsx
@@ -0,0 +1,25 @@
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
+
+function FeedbackList({ feedback }) {
+    if (!feedback || feedback.length === 0) {
+        return <p>No Feedback yet</p>;
+    }
+    return (
+        <div className='feedback-list'>
+            {feedback.map((item) => (
+                <FeedbackItem key={item.id} item={item} />
+            ))}
+        </div>
+    );
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+    }),
+};
+
+export default FeedbackList;
--- javascript/backend/routes/bookRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const bookController = require("../controllers/bookController")
-
-router.post("/books", bookController.postBooks)
-router.get("/books", bookController.getBooks)
-
-module.exports = router
